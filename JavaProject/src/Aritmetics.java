3ffc9e201ecf4091f3424a492eefd7d4b719ad4f
David Klingenberg <dklingenberg@gmail.com>
2015-12-25 16:49:45 +0100

    Added JavaProject with testing java code

---
+++ JavaProject/src/Aritmetics.java
@@ -0,0 +1,22 @@
+public class Aritmetics
+{
+    public double add(double a, double b)
+    {
+        return a+b;
+    }
+
+    public double sub(double a, double b)
+    {
+        return a-b;
+    }
+
+    public double mul(double a, double b)
+    {
+        return a*b;
+    }
+
+    public double div(double a, double b)
+    {
+        return a/b;
+    }
+}
