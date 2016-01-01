7cd36074b2f112eb0941b96619ad5022b2b12201
David Klingenberg <dklingenberg@gmail.com>
2016-01-01 19:07:58 +0100

    Added system.in

--- JVM/natives/java/lang/System.cpp
+++ JVM/natives/java/lang/System.cpp
@@ -2,6 +2,7 @@
 #include "../../Declarations.h"
 #include "../../includes.h"
 #include "../io/PrintStream.h"
+#include "../io/InputStream.h"
 
 namespace java
 {
@@ -23,16 +24,24 @@ namespace java
 				Field* errField = new Field((int)FieldAccessFlags::STATIC | (int)FieldAccessFlags::PUBLIC, Utf8String("err"), Utf8String("Ljava/io/PrintStream;"));
 				system->addField(errField);
 
-                                byte* printStrMemory = runtime->heap->allocateOnSystemMemory(sizeof(java::io::PrintStream));
+				Field* inField = new Field((int)FieldAccessFlags::STATIC | (int)FieldAccessFlags::PUBLIC, Utf8String("in"), Utf8String("Ljava/io/InputStream;"));
+				system->addField(inField);
+
+				byte* printStrMemory = runtime->heap->allocateOnSystemMemory(sizeof(java::io::PrintStream));
 				java::io::PrintStream * out = new(printStrMemory) java::io::PrintStream(&std::cout);
 				size_t outIndex = runtime->objectTable->insert(out);
 
-                                byte* errStrMemory = runtime->heap->allocateOnSystemMemory(sizeof(java::io::PrintStream));
+				byte* errStrMemory = runtime->heap->allocateOnSystemMemory(sizeof(java::io::PrintStream));
 				java::io::PrintStream * err = new(errStrMemory) java::io::PrintStream(&std::cerr);
 				size_t errIndex = runtime->objectTable->insert(err);
 
+				byte* inStrMemory = runtime->heap->allocateOnSystemMemory(sizeof(java::io::InputStream::InputStream));
+				java::io::InputStream::InputStream * in = new(inStrMemory) java::io::InputStream::InputStream(&std::cin);
+				size_t inIndex = runtime->objectTable->insert(in);
+
 				system->staticVariablesValues->set(field->fieldIndex, (word)makeReferenceAddress(outIndex));
 				system->staticVariablesValues->set(errField->fieldIndex, (word)makeReferenceAddress(errIndex));
+				system->staticVariablesValues->set(inField->fieldIndex, (word)makeReferenceAddress(inIndex));
 
 				return system;
 			};
